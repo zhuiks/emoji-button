ff86d72bc52c12e18051a0e0f95d04a9d76cd90d
zhuiks <zhuke@ukr.net>
2021-06-21 19:28:47 -0500

    add Romanian annotation data

--- vendor/cldr-json/local-config.sh
+++ vendor/cldr-json/local-config.sh
@@ -5,7 +5,7 @@
 TYPES="annotations annotationsDerived"
 
 # Only these files
-MATCH='de|en|es|fr|nl|ru|sk|uk'
+MATCH='de|en|es|fr|nl|ro|ru|sk|uk'
 
 # override the version number of CLDR
 VERSION="39.0"
